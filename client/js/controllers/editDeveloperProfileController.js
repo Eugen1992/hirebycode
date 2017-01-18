1b16574c7475c252f7c955137d01bf5719c83b5d
Eugene Alforov <eugenalforov@gmail.com>
2017-01-18 23:26:50 +0200

    added motification about profile status and redesigned edit profile page

--- client/js/controllers/editDeveloperProfileController.js
+++ client/js/controllers/editDeveloperProfileController.js
@@ -8,7 +8,10 @@
       $scope.info = info;
     });
     $scope.submit = function () {
-      userService.updateDeveloperDetails($scope.info, $scope.newAvatar);
+      userService.updateDeveloperDetails($scope.info, $scope.newAvatar)
+        .then(function(userInfo) {
+          $scope.info = userInfo;
+        });
     }
     $scope.clearAvatar = function () {
       $scope.newAvatar = null;
--- server/services/user/get-active-developers.service.js
+++ server/services/user/get-active-developers.service.js
@@ -3,7 +3,8 @@ const User = require('../../models/user');
 module.exports = function getActiveDevelopers () {
   const sQuery = {
     type: 'developer',
-    'repos.0': {$exists: true}
+    'repos.0': {$exists: true},
+    'profileReadyForPublic': true
   };
   return User.find(sQuery).then((developers) => {
     return developers;

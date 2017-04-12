14e258fe5cc30ebe3f3deaf841a73b08bb831225
Eugene Alforov <eugenalforov@gmail.com>
2017-04-13 02:20:25 +0300

    replacing cloudinary images with secure one

--- server/services/image/get-user-image-url-cloudinary.js
+++ server/services/image/get-user-image-url-cloudinary.js
@@ -4,5 +4,5 @@ module.exports = function getImageUrlCloudinary (fileName) {
   if (!fileName) {
     return '/client/assets/images/placeholder.png';
   }
-  return cloudinary.url(`folder-name/${fileName}`);
+  return cloudinary.url(`folder-name/${fileName}`).replace('http://', '//');
 }

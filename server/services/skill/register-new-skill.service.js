aa22bab81aa25171cbdb20f1cbdc52e58f804b00
Eugene Alforov <eugenalforov@gmail.com>
2017-05-12 13:26:38 +0300

    fixed duplicationg ids

--- server/services/skill/register-new-skill.service.js
+++ server/services/skill/register-new-skill.service.js
@@ -2,17 +2,35 @@ const Skill = require('../../models/skill');
 const Promise = require('bluebird');
 
 module.exports = function (skills) {
-  const newSkills = skills.filter((skill) => {
+  const skillsWithoutId = skills.filter((skill) => {
     return  !skill._id;
   });
 
-  const oldIds = skills.reduce((ids, skill) => {
-    return skill._id ? [...ids, skill._id] : ids;
-  }, []);
-  if (newSkills.length) {
-    return Skill.collection.insert(newSkills).then((result) => {
-      result.insertedIds.shift()
-      return [...oldIds, ...result.insertedIds];
+  if (skillsWithoutId.length) {
+    return Promise.all(skillsWithoutId.map((skillWithoutId) => {
+      return Skill.findOne({ name: skillWithoutId.name }).then((skill) => {
+        if (skill) {
+          return skill;
+        } else {
+          return skillWithoutId;
+        } 
+      });
+    }))
+    .then((skillsWithoutId) => {
+      const newSkills = skillsWithoutId.filter((skill) => {
+        return  !skill._id;
+      });
+      const oldIds = [...skillsWithoutId, ...skills].reduce((ids, skill) => {
+        return skill._id ? [...ids, skill._id] : ids;
+      }, []);
+      if (newSkills.length) {
+        return Skill.collection.insert(newSkills).then((result) => {
+          result.insertedIds.shift();
+          return [...oldIds, ...result.insertedIds];
+        });
+      } else {
+        return Promise.resolve(oldIds);
+      }
     });
   } else {
     return Promise.resolve(oldIds);
